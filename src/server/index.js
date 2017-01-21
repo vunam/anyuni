7ae582d82348b59b29cd5b83e89d1a3008a7fec2
Vu Nam Nguyen <vunam281@gmail.com>
2017-01-21 22:01:46 +0000

    Add eslint setup

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,12 @@
 import Koa from 'koa'
+
 const app = new Koa()
 
 app.use((ctx, next) => {
   ctx.body = 'hello anyuni'
+  next()
 })
 
-app.listen(3000)
+app.listen(3000, () => {
+  console.log('server started on 3000') // eslint-disable-line
+})
