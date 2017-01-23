4fa73e755b9e347b9844005426740077dd1187e6
Vu Nam Nguyen <vunam281@gmail.com>
2017-01-23 23:06:40 +0000

    Add initial search component

--- src/app/components/common/SearchHero/SearchHero.jsx
+++ src/app/components/common/SearchHero/SearchHero.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+
 const style = process.browser ? require('./SearchHero.css') : {}
 
-const SearchHero = () => (
-  <div className={style.SearchHero}>
-    <input type="text" />
-  </div>
-)
+class SearchHero extends React.Component {
+  componentDidMount() {
+    this.textInput.focus()
+  }
+
+  render() {
+    return (
+      <section className={style.component}>
+        <div className={style.searchWrapper}>
+          <input
+            className={style.searchInput}
+            ref={((el) => { this.textInput = el })}
+            type="text"
+            placeholder="What course are you looking for?"
+          />
+        </div>
+      </section>
+    )
+  }
+}
 
 export default SearchHero
