d11b0bd70d627fbef0ddf6a58bece644d3395cc7
Vu Nam Nguyen <vunam281@gmail.com>
2017-01-28 18:11:21 +0100

    Add redux-dev-tools

--- src/app/redux/configureStore.js
+++ src/app/redux/configureStore.js
@@ -1,10 +1,14 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import reducers from './reducers'
+
+const composeEnhancers = process.browser && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 export default function configureStore(preloadedState) {
   return createStore(
     reducers,
     preloadedState,
-    applyMiddleware()
+    composeEnhancers(
+      applyMiddleware()
+    )
   )
 }
