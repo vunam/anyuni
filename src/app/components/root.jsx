ef8843261177d72f16b46e743e7fced8e50213b2
Vu Nam Nguyen <vunam281@gmail.com>
2017-01-28 18:19:28 +0100

    Add react routing middleware

--- src/app/components/root.jsx
+++ src/app/components/root.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Router, browserHistory } from 'react-router'
 import { Provider } from 'react-redux'
+import { syncHistoryWithStore } from 'react-router-redux'
 
 const Root = (renderProps) => {
+  const { store } = renderProps
+  const history = process.browser && store ? syncHistoryWithStore(browserHistory, store) : browserHistory
   return (
-    <Provider store={renderProps.store}>
+    <Provider store={store}>
       <Router history={browserHistory} {...renderProps} />
     </Provider>
   )
--- src/app/redux/reducers.js
+++ src/app/redux/reducers.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux'
+import { routerReducer } from 'react-router-redux'
+
 import searchhero from '../components/common/SearchHero/dux'
 import { reducer as formReducer } from 'redux-form'
 
 const reducers = combineReducers({
+  routing: routerReducer,
   form: formReducer,
   searchhero
 })
