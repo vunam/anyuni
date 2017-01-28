f73486fb1b98ff76dd74fbd28ecf68c70aec4f8f
Vu Nam Nguyen <vunam281@gmail.com>
2017-01-28 19:07:35 +0100

    Add clear button to search hero

--- src/app/components/common/SearchHero/SearchHero.jsx
+++ src/app/components/common/SearchHero/SearchHero.jsx
@@ -1,32 +1,60 @@
 import React from 'react'
-import { reduxForm, Field } from 'redux-form'
+import { connect } from 'react-redux'
+import { reduxForm, Field, reset, focus, Form } from 'redux-form'
 
 const style = process.browser ? require('./SearchHero.css') : {}
 
 class SearchHero extends React.Component {
   componentDidMount() {
-    this.textInput.focus()
+  }
+
+  getValue = () => {
+    const { form } = this.props
+    if(form && form.values && form.values.searchInput) return form.values.searchInput
+    return null
+  }
+
+  clearSearchInput = (e) => {
+    e.preventDefault()
+    this.props.reset()
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
   }
 
   render() {
     return (
       <section className={style.component}>
         <div className={style.searchWrapper}>
-          <form>
+          <Form className={style.searchForm} onSubmit={this.handleSubmit}>
+            { this.getValue() && <button className={style.clearField} type="button" onClick={ this.clearSearchInput }>Clear field</button> }
             <Field
               className={style.searchInput}
+              ref={(el) => {this.searchInput = el}}
               component="input"
               type="text"
-              name="search"
+              name="searchInput"
               placeholder="What course are you looking for?"
+              autoComplete="off"
             />
-          </form>
+          </Form>
         </div>
       </section>
     )
   }
 }
 
+const mapStateToProps = state => ({
+  form: state.form.search
+})
+
+const mapDispatchToProps = dispatch => ({
+  focus: () => dispatch(focus('search', 'searchInput')),
+  reset: () => dispatch(reset('search'))
+})
+
+SearchHero = connect(mapStateToProps, mapDispatchToProps)(SearchHero)
 SearchHero = reduxForm({ form: 'search' })(SearchHero)
 
 export default SearchHero
